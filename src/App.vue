f7506554f9208477a34fd5d3ae8c05c4a131e917
CoderSerio <CoderSerio@2779066456@qq.com>
2023-04-12 20:43:14 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Vite + Vue" />
+  <Test />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-button type="success">Success</el-button>
+</template>
+
+<script setup lang="ts">
+  console.log(123)
+</script>
+
+<style>
+  .wrapper {
+    background-color: red;
+  }
+</style>
