9aa57189c9a50840fd32162ec634452fa0f9f94c
Tosuke <you@example.com>
2023-04-13 16:41:58 +0800

    feat: 测试

---
+++ src/pages/common/ATaksOrder/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+<template>
+<div>666</div>
+</template>
+<style>
+
+</style>
--- src/pages/test-page/index.vue
+++ src/pages/test-page/index.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
 import Demo1 from '@/pages/common/demo1/index.vue'
 import Demo2 from '@/pages/common/demo2/index.vue'
+import Atakskorder from "@/pages/common/ATaksOrder/index.vue"
 </script>
 
 <template>
   <div>这是测试开发页面，把你的组件写好了扔在这里就好</div>
   <Demo1 />
   <Demo2 />
+  <Atakskorder />
 </template>
-
-
 
 <style scoped>
 
