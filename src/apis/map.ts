5e7f9e78a0415ffbd35e1971c174813444eb94fd
Tosuke <you@example.com>
2023-04-16 10:52:25 +0800

    feat：更新地图api，完善地图功能

--- src/apis/map.ts
+++ src/apis/map.ts
@@ -1,16 +1,51 @@
 import request from "@/utils/request";
+import { resolve } from "path";
 
 const BASE_URL = "https://geo.datav.aliyun.com/areas_v3/bound";
-
-const cn2number = {
+interface map{
+	[propName: string]: number
+}
+const cn2number:map = {
 	成都市: 510100,
 	都江堰市: 510181,
+	彭州市:510182,
+	金堂县:510121,
+	简阳市:510121,
+	双流区:510116,
+	新津区:510118,
+	蒲江县:510131,
+	邛崃市:510183,
+	大邑县:510129,
+	崇州市:510184,
+	青白江区:510113,
+	龙泉驿区:510112,
+	成华区:510108,
+	锦江区:510104,
+	武侯区:510107,
+	青羊区:510105,
+	金牛区:510106,
+	郫都区:510117,
+	温江区:510115,
+	新都区:510114
 };
 
 /** 获取 成都市 地图JSON数据 */
-export const getMapData: any = async () => {
+export const getCdData: any =  async () => {
 	const data = await request.get(`${BASE_URL}/510100_full.json`);
-	return data;
+	return data
+}
+export const getMapData: any = async (name:string) => {
+	let num = cn2number[name]
+	if(name=="成都市"){
+		const data = await request.get(`${BASE_URL}/${num}_full.json`);
+		return data;
+	}
+	else{
+		const data = await request.get(`${BASE_URL}/${num}.json`);
+		return data;
+	}
+	
+	
 };
 
 /** 获取 都江堰 地图JSON数据 */
