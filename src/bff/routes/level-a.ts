eeed3b44fab5f53255f69d7df837345aee45a2ae
lhuanran <3156272973@qq.com>
2023-04-19 02:54:08 +0800

    新增5个接口

---
+++ src/bff/routes/level-a.ts
@@ -0,0 +1,126 @@
+import express, { Request, Response, Router } from "express";
+import {
+	readFileListData,
+	readTaskListData,
+	writeFileListData,
+	writeTaskListData,
+} from "../utils/file";
+import { getQueryPair } from "../utils/query";
+
+const levelA = express.Router();
+
+// A文件上传
+levelA.get("/upload-file", (request, response) => {
+	const binaryData = readFileListData();
+	const data = JSON.parse(binaryData.toString());
+
+	// TODO: 先随便写点吧
+	const id = Math.floor(1000 * Math.random());
+	const demo = {
+		fileId: `${id}`,
+		fileName: `测试文件${id}`,
+		from: "A123",
+		to: "", // 开始是空的，表示没有B执行
+		data: {
+			title: "这是任务内容示意, 文件格式待定——确定后再补充相关功能",
+		},
+	};
+
+	data.push(demo);
+	writeFileListData(JSON.stringify(data));
+
+	response.send(
+		JSON.stringify({
+			status: 200,
+			msg: "",
+		})
+	);
+});
+
+// A查看文件列表
+levelA.get("/get-file-list", (request: Request, response: Response) => {
+	const binaryData = readFileListData();
+	response.send(
+		JSON.stringify({
+			status: 200,
+			msg: "",
+			data: {
+				hasMore: true,
+				total: 100,
+				list: JSON.parse(binaryData),
+			},
+		})
+	);
+});
+
+// A下发文件
+levelA.get("/dispatch-file", (request, response) => {
+	const queryPair = getQueryPair(request.url);
+	const binaryData = readFileListData();
+	const data = JSON.parse(binaryData);
+	const newData = data.map((item: any) =>
+		item?.fileId == queryPair.fileId
+			? {
+					...item,
+					to: "B456",
+			  }
+			: item
+	);
+	writeFileListData(JSON.stringify(newData));
+	response.send(
+		JSON.stringify({
+			status: 200,
+			msg: "",
+		})
+	);
+});
+
+levelA.get("/test", (request: Request, response: Response) => {
+	response.send("这是响应的内容");
+});
+
+//文件删除(没测过)
+levelA.get("/delete-file", (request, response) => {
+	const binaryData = readFileListData();
+	const data = JSON.parse(binaryData.toString());
+	const queryPair = getQueryPair(request.url);
+	const num = data.findIndex((obj: any) => {
+		return obj.fileId === queryPair.fileId;
+	});
+
+	data.splice(num, 1);
+
+	writeFileListData(JSON.stringify(data));
+
+	response.send(
+		JSON.stringify({
+			status: 200,
+			msg: "",
+		})
+	);
+});
+
+//查看进度列表(应该过了)
+levelA.get("/get-progress-list", (request, response) => {
+	const binaryData = readFileListData();
+	const data = JSON.parse(binaryData.toString());
+	const queryPair = getQueryPair(request.url);
+	const newdata = data.filter((obj: any) => {
+		return obj.from === "A123";
+	});
+
+	
+	response.send(
+		JSON.stringify({
+			status: 200,
+			msg: "",
+			data: {
+				hasMore: true,
+				total: 100,
+				list: newdata,
+			},
+		})
+	);
+});
+
+export default levelA;
---
+++ src/bff/routes/level-b.ts
@@ -0,0 +1,159 @@
+import express, { Request, Response, Router } from "express";
+import { readFileListData, readTaskListData, writeTaskListData } from "../utils/file";
+import log from "../utils/log";
+import { getQueryPair } from "../utils/query";
+
+const levelB = express.Router();
+
+// 测试1
+levelB.get("/test", (request: Request, response: Response) => {
+	response.send("test!!!");
+});
+
+// 创建任务
+levelB.get("/create-task", (request, response) => {
+	const binaryData = readTaskListData();
+	const data = JSON.parse(binaryData.toString());
+
+	// TODO: 先随便写点吧
+	const id = Math.floor(1000 * Math.random());
+	const demo = {
+		fileId: id,
+		fileName: `测试文件${id}`,
+		from: "B456",
+		to: "", // 为空就代表还没下发
+		data: {
+			title: "这是任务内容示意, 文件格式待定——确定后再补充相关功能",
+		},
+	};
+
+	data.push(demo);
+	writeTaskListData(JSON.stringify(data));
+
+	response.send(
+		JSON.stringify({
+			status: 200,
+			msg: "",
+		})
+	);
+});
+
+// B查看文件列表
+levelB.get("/get-file-list", (request: Request, response: Response) => {
+	const binaryData = readFileListData();
+	// 目前的逻辑是to不为空就是下发了的
+	const data = JSON.parse(binaryData ?? []).filter((item: any) => item.to);
+	response.send(
+		JSON.stringify({
+			status: 200,
+			msg: "",
+			data: {
+				hasMore: true,
+				total: 100,
+				list: data,
+			},
+		})
+	);
+});
+
+// B查看任务列表
+levelB.get("/get-task-list", (request: Request, response: Response) => {
+	const binaryData = readTaskListData();
+	// 目前的逻辑是to不为空就是下发了的
+	const data = JSON.parse(binaryData ?? []).filter((item: any) => item.to);
+	response.send(
+		JSON.stringify({
+			status: 200,
+			msg: "",
+			data: {
+				hasMore: true,
+				total: 100,
+				list: data,
+			},
+		})
+	);
+});
+
+// B下发任务
+levelB.get("/dispatch-task", (request, response) => {
+	const queryPair = getQueryPair(request.url);
+	const binaryData = readTaskListData();
+	const data = JSON.parse(binaryData);
+	const newData = data.map((item: any) =>
+		item?.fileId == queryPair.fileId
+			? {
+					...item,
+					to: "789",
+			  }
+			: item
+	);
+	writeTaskListData(JSON.stringify(newData));
+});
+
+//任务删除(没测过)
+levelB.get("/delete-task", (request, response) => {
+	const binaryData = readFileListData();
+	const data = JSON.parse(binaryData.toString());
+	const queryPair = getQueryPair(request.url);
+	const num = data.findIndex((obj: any) => {
+		return obj.taskId === queryPair.taskId;
+	});
+
+	data.splice(num, 1);
+
+	writeTaskListData(JSON.stringify(data));
+
+	response.send(
+		JSON.stringify({
+			status: 200,
+			msg: "",
+		})
+	);
+});
+
+//查看进度列表(copy a的应该过了吧)
+levelB.get("/get-task-progress", (request, response) => {
+	const binaryData = readFileListData();
+	const data = JSON.parse(binaryData.toString());
+	const queryPair = getQueryPair(request.url);
+	const newdata = data.filter((obj: any) => {
+		return obj.from === queryPair.from;
+	});
+	
+	response.send(
+		JSON.stringify({
+			status: 200,
+			msg: "",
+			data: {
+				hasMore: true,
+				total: 100,
+				list: newdata,
+			},
+		})
+	);
+});
+
+//b审核c的提交(没测过)
+levelB.get("/task-pass", (request, response) => {
+	const binaryData = readFileListData();
+	const data = JSON.parse(binaryData.toString());
+	const queryPair = getQueryPair(request.url);
+	//queryPair.passanswer = "true";
+	//我们假设通过为true，可能会有两个按钮，我们假设按钮传进来的值为passanswer
+	const num = data.findIndex((obj: any) => {
+		return obj.taskId === queryPair.taskId;
+	});
+
+	if (queryPair.passanswer === "true"){
+		data[num].status += 1;
+	}
+
+	writeTaskListData(JSON.stringify(data));
+	response.send(
+		JSON.stringify({
+			status: 200,
+			msg: "",
+		})
+	);
+});
+export default levelB;
